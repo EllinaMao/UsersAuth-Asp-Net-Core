4646a085db2dc6ea93b73af4eb3e402b20868fa4
Olga <xotaoshi@gmail.com>
2026-03-04 21:47:17 +0200

    MasterLayout

--- WebApplication1/ViewModels/Notes/ChangeNoteViewModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace WebApplication1.ViewModels.Notes
-{
-    public class ChangeNoteViewModel
-    {
-    }
-}
--- WebApplication1/ViewModels/Notes/CreateNoteViewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WebApplication1.ViewModels
-{
-    public class CreateNoteViewModel
-    {
-
-    }
-}
